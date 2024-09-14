7bd4838664c34c065f5f1253934c13fc5f8abbed
Alexander Burdiss <aburdiss@icloud.com>
2024-09-14 12:42:38 -0400

    Add Sanity Studio URL

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -3,11 +3,12 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'm266lax5',
-    dataset: 'production'
+    dataset: 'production',
   },
   /**
    * Enable auto-updates for studios.
    * Learn more at https://www.sanity.io/docs/cli#auto-updates
    */
   autoUpdates: true,
+  studioHost: 'https://reloadcincy.sanity.studio',
 })
