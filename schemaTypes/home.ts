cac205c936d99760ebfc2980d1cb9d7e1e0c7551
Alexander Burdiss <aburdiss@icloud.com>
2024-09-14 16:46:26 -0400

    Add Gig Headline to home schema

--- schemaTypes/home.ts
+++ schemaTypes/home.ts
@@ -6,6 +6,11 @@ export default defineType({
   type: 'document',
   fields: [
     defineField({
+      name: 'gigsHeadline',
+      title: 'Gigs Headline',
+      type: 'string',
+    }),
+    defineField({
       name: 'firstSectionHeadline',
       title: 'First Section Headline',
       type: 'string',
