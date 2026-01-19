f159080a8d2e7717e01a7cd7e7a3fcedd5b71755
Alexander Burdiss <aburdiss@icloud.com>
2026-01-19 11:33:15 -0500

    feat: add image to homepage

--- schemaTypes/home.ts
+++ schemaTypes/home.ts
@@ -1,4 +1,4 @@
-import {defineField, defineType} from 'sanity'
+import { defineField, defineType } from 'sanity';
 
 export default defineType({
   name: 'home',
@@ -19,7 +19,22 @@ export default defineType({
       name: 'firstSectionText',
       title: 'First Section Text',
       type: 'array',
-      of: [{type: 'block'}],
+      of: [{ type: 'block' }],
+    }),
+    defineField({
+      name: 'firstSectionImage',
+      title: 'First Section Image',
+      type: 'image',
+      fields: [
+        {
+          name: 'alt',
+          type: 'string',
+          title: 'Alternative text (for screen reader accessibility)',
+          description: 'Alternative text is required.',
+          hidden: ({ parent }) => !parent?.asset,
+          validation: (Rule) => [Rule.required()],
+        },
+      ],
     }),
     defineField({
       name: 'membersSectionHeadline',
@@ -27,4 +42,4 @@ export default defineType({
       type: 'string',
     }),
   ],
-})
+});
